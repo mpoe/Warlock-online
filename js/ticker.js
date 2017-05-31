9d020e3eaca67e496e1768bf88079844557fb08f
Mpoe <Morten_p_oe@hotmail.com>
2017-05-31 12:18:25 +0200

    j

---
+++ js/ticker.js
@@ -0,0 +1,8 @@
+var Ticker ={
+    start:function(){
+        createjs.Ticker.setFPS(60);
+        createjs.Ticker.on("tick", function(e){
+            Game.stage.update(e);
+        });
+    }
+}
---
+++ js/unit.js
@@ -0,0 +1,8 @@
+var Unit = {
+	move: function(){
+
+	},
+	shoot: function(){
+
+	}
+}
