4705305449258b500df9ba4a0060e55c070711b6
Unknown <morten_p_oe@hotmail.com>
2017-07-06 01:56:10 +0200

    Working reticle + util

--- js/Util.js
+++ js/Util.js
@@ -4,5 +4,30 @@
 var Util = {
     getRandomInt:function(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+    getAngle: function(centerX,centerY,x,y){
+        //https://gamedev.stackexchange.com/questions/33709/get-angle-in-radians-given-a-point-on-a-circle
+        // Get a value between -pi to pi
+        // When converting, add 180 so the value is between 0,360 instead of -180,180
+        return Util.degrees(Math.atan2(y - centerY, x - centerX))+180;
+    },
+    getRadians:function(centerX,centerY,x,y){
+        return Math.atan2(y - centerY, x - centerX)
+    },
+    degrees: function(radians) {
+        //http://cwestblog.com/2012/11/12/javascript-degree-and-radian-conversion/
+        return radians * 180 / Math.PI;
+    },
+    getDistance: function(centerX,centerY,x,y){
+        var a = centerX - x;
+        var b = centerY - y;
+        return Math.sqrt( a*a + b*b );
+    },
+    getPointOnCircle:function(centerX,centerY,radius,radians){
+        console.log(centerX+" "+centerY+" "+radius+" "+radians);
+        var x = centerX + radius * Math.cos(radians);
+        var y = centerY + radius * Math.sin(radians);
+        return [x,y];
     }
+
 }
--- js/unit.js
+++ js/unit.js
@@ -1,7 +1,8 @@
 var Unit = {
+	unitRadius: 50,
 	createUnit: function(gid, color,x,y){
 		var unit = new createjs.Shape();
-		unit.graphics.beginFill(color).drawCircle(0, 0, 50);
+		unit.graphics.beginFill(color).drawCircle(0, 0, Unit.unitRadius);
 		//The centerpoint of the circle is the x and y values
 		unit.x = x;
 		unit.y = y;
@@ -10,13 +11,19 @@ var Unit = {
 
 		Game.stage.addChild(unit);
 
-		var reticle = new createjs.Bitmap(Preloader.queue.getResult("img/reticle.png"))
+		//var reticle = new createjs.Bitmap(Preloader.queue.getResult("img/reticle.png"))
+		var reticle = new createjs.Shape();
+
+		reticle.graphics.setStrokeStyle(4);
+		reticle.graphics.beginStroke("Green").drawCircle(0,0,5);
+
+		//var g = new createjs.Graphics();g.setStrokeStyle(1);g.beginStroke("#000000");g.drawCircle(0,0,30);
 		reticle.x=x;
 		reticle.y=y;
 		unit.reticle = reticle;
 
-		reticle.scaleY=0.1;
-		reticle.scaleX=0.1;
+		//reticle.scaleY=0.1;
+		//reticle.scaleX=0.1;
 
 		Game.stage.addChild(reticle);
 
@@ -32,6 +39,7 @@ var Unit = {
 			if(unitid == Game.units[i].gamepadID){
 				Game.units[i].x+=x;
 				Game.units[i].y+=y;
+				Unit.reticleFollow(Game.units[i].gamepadID,x,y);
 			}
 		}
 
@@ -39,8 +47,38 @@ var Unit = {
 	moveReticle: function(unitid,x,y){
 		for( var i = 0; i<Game.units.length;i++){
 			if(unitid == Game.units[i].gamepadID){
-				Game.units[i].reticle.x+=x;
-				Game.units[i].reticle.y+=y;
+				var unit = Game.units[i]
+				var distance = Util.getDistance(unit.x,unit.y,unit.reticle.x,unit.reticle.y);
+				if(distance > 150 || distance <-150){
+					console.log("too far away");
+				}else{
+					//unit.reticle.x+=x;
+					//unit.reticle.y+=y;
+
+					var radians = Util.getRadians(0,0,x,y);
+
+					//console.log(Util.getPointOnCircle(unit.x,unit.y,Unit.unitRadius,unit.reticle.x,unit.reticle.y));
+					var aCoords = Util.getPointOnCircle(unit.x,unit.y,Unit.unitRadius*2,radians)
+
+					unit.reticle.x=aCoords[0];
+					unit.reticle.y=aCoords[1];
+
+
+
+
+				}
+
+
+
+			}
+		}
+	},
+	reticleFollow: function(unitid,x,y){
+		for( var i = 0; i<Game.units.length;i++){
+			if(unitid == Game.units[i].gamepadID){
+				var unit = Game.units[i]
+				unit.reticle.x+=x;
+				unit.reticle.y+=y;
 			}
 		}
 	},
